0623c567f1ddafce64a9b04230fd188b12c42d23
Osher Lerner <oshlern@nuevaschool.org>
2018-10-27 11:30:37 -0700

    using ipython and networkx for centrality

--- circuits2.py
+++ circuits2.py
@@ -1,15 +1,8 @@
 from abc import ABC, abstractmethod
 
 class Node(ABC):
-    wires_in, wires_out = [], []
+    ins, outs = [], []
 
     def __init__(wires_in, wires_out)
-
-class Wire:
-    resistance = 0
-
-    def __init__(self, nodes=[]):
-        self.nodes = nodes
-
 class Resistor(Node):
 
